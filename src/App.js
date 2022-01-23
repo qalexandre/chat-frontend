4a5fb0849aa3e62684635171f3ad2253b405ed7d
qalexandre <alexandre.goncalves33@outlook.com>
2022-01-23 17:46:50 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Join />} />
+      <Route path="/chat" element={<Chat/>} component={<Chat />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
